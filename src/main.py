dfe8755f34f255c4ba6d197efef521444c0d1d3c
NolanBrechtel <ndbrechtel@gmail.com>
2024-08-29 21:12:47 -0500

    Began Project

--- src/main.py
+++ src/main.py
@@ -5,6 +5,8 @@ from tokens import API_KEY
 
 TESTING_GUILD_ID = 1192706822415597568  # Replace with your guild ID
 
+PERMITTED_IDS = [257989883707129859]  # for running the /terminal command
+
 bot = commands.Bot()
 
 
@@ -13,9 +15,12 @@ async def on_ready():
     print(f'We have logged in as {bot.user}')
 
 
-@bot.slash_command(description="My first slash command", guild_ids=[TESTING_GUILD_ID])
-async def hello(interaction: nextcord.Interaction):
-    await interaction.send("Hello!")
+@bot.slash_command(description='Run command in terminal', guild_ids=[TESTING_GUILD_ID])
+async def terminal(interaction: nextcord.Interaction):
+    if interaction.user.id in PERMITTED_IDS:
+        await interaction.send('Permitted!')
+    else:
+        await interaction.send('Not Permitted.')
 
 
 bot.run(API_KEY)
