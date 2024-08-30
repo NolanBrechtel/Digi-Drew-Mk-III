3fc8b2f86ff04fcd0da43ed205cacbc3495af02c
NolanBrechtel <ndbrechtel@gmail.com>
2024-08-29 20:57:45 -0500

    Began Project

--- src/main.py
+++ src/main.py
@@ -1,16 +1,21 @@
 import nextcord
 from nextcord.ext import commands
 
-TESTING_GUILD_ID = 123456789  # Replace with your guild ID
+from tokens import API_KEY
+
+TESTING_GUILD_ID = 1192706822415597568  # Replace with your guild ID
 
 bot = commands.Bot()
+
 
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
 
+
 @bot.slash_command(description="My first slash command", guild_ids=[TESTING_GUILD_ID])
 async def hello(interaction: nextcord.Interaction):
     await interaction.send("Hello!")
 
-bot.run('your token here')
+
+bot.run(API_KEY)
